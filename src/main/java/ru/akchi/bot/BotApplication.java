c9673a22a2eb5839f8ad63e8692984809fd394b9
akchi724 <akchi724@gmail.com>
2022-10-11 00:22:06 +0300

    bot withoutSpringVesion

--- src/main/java/ru/akchi/bot/BotApplication.java
+++ src/main/java/ru/akchi/bot/BotApplication.java
@@ -1,13 +1,23 @@
 package ru.akchi.bot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.akchi.bot.service.TelegramBot;
 
-@SpringBootApplication
 public class BotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BotApplication.class, args);
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new TelegramBot(
+                    "MyJava19890Bot",
+                    "5613951667:AAHC4i1uU5QsrWC2vFO9ellI0ToPHI8Z_c0"));
+        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/ru/akchi/bot/service/TelegramBot.java
+++ src/main/java/ru/akchi/bot/service/TelegramBot.java
@@ -2,29 +2,53 @@ package ru.akchi.bot.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+//import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+//import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ru.akchi.bot.property.BotConfig;
 
 @Slf4j
-@Service
-@RequiredArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
 
-    private final BotConfig botConfig;
+    private  final String botName;
+    private  final String botApiToken;
+
+    public TelegramBot(String botName, String botApiToken) {
+        this.botName = botName;
+        this.botApiToken = botApiToken;
+    }
+
 
     @Override
     public String getBotUsername() {
-        return botConfig.getBotName();
+        return botName;
     }
+
+//    @Override
+//    public void processNonCommandUpdate(Update update) {
+//        if (update.hasMessage() && update.getMessage().hasText()) {
+//            String messageText = update.getMessage().getText();
+//            Long chatId = update.getMessage().getChatId();
+//            String userName = update.getMessage().getChat().getFirstName();
+//            String answer = "";
+//            switch (messageText) {
+//                case "/start":
+//                    answer = startCommandReceived(chatId, userName);
+//                    break;
+//                default:
+//                    answer = "Sorry, command wasn't recognized";
+//            }
+//            sendMessage(chatId, answer);
+//        }
+//
+//    }
 
     @Override
     public String getBotToken() {
-        return botConfig.getToken();
+        return botApiToken;
     }
 
     @Override
